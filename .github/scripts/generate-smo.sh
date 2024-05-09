9cdd857cb082c63c331b3204ac9f6a1309bb7180
MonsterDruide1 <5958456@gmail.com>
2024-05-09 14:30:13 +0200

    Add script

---
+++ .github/scripts/generate-smo.sh
@@ -0,0 +1,12 @@
+HEADER=include-all.cpp
+echo "#ifndef __ALL_HEADERS__" > $HEADER
+echo "#define __ALL_HEADERS__" >> $HEADER
+# don't check NintendoSDK, as that contains 4.0.1-only files
+for file in $(find $@ -name '*.h')
+do
+    # ignore Wii-specific files
+    # ignore ScopeGuard, as that uses <optional> (C++17)
+    if [[ $file == *"cafe"* || $file == *"seadScopeGuard.h" ]]; then continue; fi
+    echo "#include \"$file\"" >> $HEADER
+done
+echo "#endif" >> $HEADER
